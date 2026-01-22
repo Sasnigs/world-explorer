c998d6bb3dbdec51ff452c709da4c37cb8180f9e
Sasnigs <88667585+Sasnigs@users.noreply.github.com>
2026-01-21 19:17:44 -0600

    styled CountryGrid and countrycard

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,7 +1,19 @@
-export default function CountryCard(){
-    return(
-        <>
-       
-        </>
+export default function CountryCard({ country }) {
+    return (
+        <div className="w-48 rounded-xl border border-gray-200 bg-purple-50 p-4 shadow-lg shadow-indigo-500/50  hover:scale-105 transition cursor-pointer">
+            <img
+                className="h-24 w-full rounded-md"
+                src={country.flags.png}
+                alt={`${country.name.common} flag`}
+            />
+            <div className="mt-3 space-y-1">
+                <p className="text-sm font-semibold text-gray-900">{country.name.common}</p>
+                <p className="text-xs text-gray-600">{country.region}</p>
+                <hr className="my-2 h-px border-0 " />
+                <p className="text-xs text-gray-600">
+                    Capital: {country.capital?.[0] ?? "N/A"}
+                </p>
+            </div>
+        </div>
     )
 }
--- src/components/CountryGrid.jsx
+++ src/components/CountryGrid.jsx
@@ -1,10 +1,11 @@
 import CountryCard from "./CountryCard"
-export default function CountryGrid({countries}){
-    return(
-        <>
-            {countries.map(country => (
-                <CountryCard key={country.cca3} country={country} />
-            ))}
-        </>
-    )
+export default function CountryGrid({ countries }) {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 overflow-x-hidden p-5 max-w-7xl mx-auto
+">
+      {countries.map(country => (
+        <CountryCard key={country.cca3} country={country} />
+      ))}
+    </div>
+  )
 }
